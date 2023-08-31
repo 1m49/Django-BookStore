f64679a96e5261c925edca11d77aca718f22d2d0
imanakbari <89096381+1m49@users.noreply.github.com>
2023-08-31 13:15:34 +0330

    add BookDetailView

--- books/views.py
+++ books/views.py
@@ -3,7 +3,15 @@ from django.views import generic
 from .models import Book
 
 
+# show list of books
 class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+
+
