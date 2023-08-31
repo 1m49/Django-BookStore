8fe21cc85de3fb638995d52a2ac21bd4c8e09391
imanakbari <89096381+1m49@users.noreply.github.com>
2023-08-31 13:55:23 +0330

    add BookUpdateView & BookDeleteView

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Book
+from django.urls import reverse_lazy
 
 
 # show list of books
@@ -21,3 +22,18 @@ class BookCreateView(generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price']
     template_name = 'books/book_create.html'
+
+
+# update books
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
+    success_url = reverse_lazy('book_list')
+
+
+# delete books
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
