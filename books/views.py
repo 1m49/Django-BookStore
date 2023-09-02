23b35a58ed6f6fc1b5d43bf955563d927a800085
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-02 19:04:10 +0330

    Add permission to users to edit or delete books they created themselves

--- books/views.py
+++ books/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
 from .models import Book
@@ -56,15 +56,23 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
 
 
 # update books
-class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'cover', 'author', 'translator', 'publisher', 'description', 'price', ]
     template_name = 'books/book_update.html'
     success_url = reverse_lazy('book_list')
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
 
 # delete books
-class BookDeleteView(LoginRequiredMixin, generic.DeleteView):
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
