f1000e84d4f1e98097fd449b07d97208038281d0
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-02 14:08:05 +0330

    To better Comment model

--- books/models.py
+++ books/models.py
@@ -26,6 +26,8 @@ class Comment(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
 
     def __str__(self):
         return self.text
