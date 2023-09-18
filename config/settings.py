fdc119bcbd405007b04a01ec343ce64c070dfedc
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-18 17:52:25 +0330

    final changes

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-ao((34wuo5n7ck^1)m*hvu6&7tlc09ua=&=4#69(^vs$l6ctzw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = []
 
 # Application definition
 
