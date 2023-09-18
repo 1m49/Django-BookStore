524de9a65345d249cc1e8bd0c4c921f582a933cf
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-18 11:22:33 +0330

    config files to deploy project in vercel

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao((34wuo5n7ck^1)m*hvu6&7tlc09ua=&=4#69(^vs$l6ctzw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
